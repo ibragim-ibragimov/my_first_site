e43f2543af9b438b2cceead3db56a26a8ddacefa
Andrew Chernobrov <a-chernobrov1985@gmail.com>
2022-08-18 10:00:05 +0200

    my_first_site

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 import os
-from flask import Flask, redirect, url_for
+import subprocess
+import rp
+
+
+from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return redirect("http://example.com", code=302)
+def index():
+    
+    return render_template('index.html')
+
+@app.route("/dns", methods = ['GET', 'POST'])
+def page():
+
+    address = None
+
+    if request.method == 'POST':
+        address = request.form['address']
+        cmd = 'dig a ' + str(address)
+    return """
+     <html>
+       <body>""" + "Result:\n<br>\n" + (str(subprocess.check_output(cmd, shell=True)) if address else "") + """
+          <form action = "/dns" method = "POST">
+             <p><h3>Enter address</h3></p>
+             <p><input type = 'text' name = 'address'/></p>
+             <p><input type = 'submit' value = 'Lookup'/></p>
+          </form>
+       </body>
+    </html>
+    """
 
 
 if __name__ == '__main__':
